444e2079a09661917fb4ed620b74a43263c7ab52
Malphegal <nateag_96@hotmail.fr>
2017-02-23 21:48:21 +0100

    gestion v1

--- HashCode/HashCode/gestionFichier.cs
+++ HashCode/HashCode/gestionFichier.cs
@@ -6,26 +6,64 @@ using System.Threading.Tasks;
 
 namespace HashCode
 {
-    class gestionFichier
+    static class gestionFichier
     {
-        /*
-           // --------------
-                // Lire fichier
-                // --------------
+            // Méthode
 
-            Console.WriteLine("test");
-            Console.ReadKey(true);
+            // --------------
+            // Lire fichier
+            // --------------
 
+        public static void lectureFichier(string adresseFichier) // "C:\Users\Anacin\Downloads\kittens.in"
+        {
             string[] text = (System.IO.File.ReadAllText(@"C:\Users\Anacin\Downloads\kittens.in")).Split('\n');
-            string[] ligne1
+            string[] ligne1 = text[0].Split(' ');
+            string[] ligne2 = text[1].Split(' ');
+            string ligneCouranteEP;
 
-            foreach (string s in text)
-            {
-                Console.WriteLine("NB vidéo : " + text[0].Split(' ')[0]);
+                // ---------------------------------
+                // Création des vidéos dans le main
+                // ---------------------------------
+
+            foreach (string s in ligne2)
+                Program.videos.Add(new Video(int.Parse(s)));
+
+                // ---------------------------------
+                // Création des caches
+                // ---------------------------------
+
+            for (int i = 0; i < int.Parse(ligne1[3]);)
+                Program.caches.Add(new Cache(i++, int.Parse(ligne1[4])));
+
+                // ---------------------------------
+                // Création des EP
+                // ---------------------------------
+            
+            int indiceLigneCourante = 2;
+            List<int> nbLigneCacheCourant = new List<int>();
+            int numeroEPActuel = 0;
+
+            for (int i = 0; i < int.Parse(ligne1[1]); i++) { // Pour chaque endPoint
+
+                foreach (int integer in nbLigneCacheCourant) // Ligne courante += ligneCacheCourante
+                    indiceLigneCourante += integer;
+                ligneCouranteEP = text[indiceLigneCourante];
+
+                nbLigneCacheCourant.Add(int.Parse(ligneCouranteEP.Split(' ')[1])); // ADD nombre cache du EP courant
+
+                for (int j = indiceLigneCourante; j < nbLigneCacheCourant.Last() + indiceLigneCourante; j++) // Chaque cache d'un EP
+                {
+                    Program.EP.Add(new endPoint(
+                        numeroEPActuel++,
+                        int.Parse(ligneCouranteEP.Split(' ')[1]),
+                        new int[int.Parse(ligne1[0])],
+                        int.Parse(ligneCouranteEP.Split(' ')[0]),
+                        new int[int.Parse(ligneCouranteEP.Split(' ')[1])])
+                        );
+                    for (int k = 0; k < int.Parse(ligneCouranteEP.Split(' ')[1]); k++)
+                        Program.EP.Last().latCache[k] = int.Parse(text[indiceLigneCourante + k].Split(' ')[1]);
+                }
             }
-
-                // --------------
-                // Écrire dans fichier
-    */
-    }
+        } // Fin méthode lecture
+    } // FIn class
 }
