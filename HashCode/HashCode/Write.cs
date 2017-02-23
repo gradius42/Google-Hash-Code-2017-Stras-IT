bb6e24c6030c0feb2af397a9acd2cb8f1b7ad53e
gradius42 <geoffrey.haon@orange.fr>
2017-02-23 21:52:37 +0100

    Write.cs

---
+++ HashCode/HashCode/Write.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode
+{
+    class Write
+    {
+        static void writeOutData(String s)
+        {
+            File.Create("./"+ s);
+            using (StreamWriter outputFile = new StreamWriter("./"+s))
+            {
+                outputFile.Write("" + Cache.nbUsed + "\n");
+
+                foreach (Cache c in Program.caches)
+                {
+                    if (c.used)
+                    {
+                        string line = "" + c.id;
+                        
+                        for(int i = 0; i < c.lVideo.Count; i++)
+                        {
+                            line += " " + c.lVideo[i];
+                        }
+
+                        line += "\n";
+
+                        outputFile.Write(line);
+                    }
+
+                }
+            }
+        }
+
+    }
+}
