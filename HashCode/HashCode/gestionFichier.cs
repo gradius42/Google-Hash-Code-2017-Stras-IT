24863d24e36a67d5d2b3e2b99e727600a2432d4c
Malphegal <nateag_96@hotmail.fr>
2017-02-23 20:23:14 +0100

    vbjb

--- HashCode/HashCode/gestionFichier.cs
+++ HashCode/HashCode/gestionFichier.cs
@@ -8,5 +8,24 @@ namespace HashCode
 {
     class gestionFichier
     {
+        /*
+           // --------------
+                // Lire fichier
+                // --------------
+
+            Console.WriteLine("test");
+            Console.ReadKey(true);
+
+            string[] text = (System.IO.File.ReadAllText(@"C:\Users\Anacin\Downloads\kittens.in")).Split('\n');
+            string[] ligne1
+
+            foreach (string s in text)
+            {
+                Console.WriteLine("NB vidéo : " + text[0].Split(' ')[0]);
+            }
+
+                // --------------
+                // Écrire dans fichier
+    */
     }
 }
