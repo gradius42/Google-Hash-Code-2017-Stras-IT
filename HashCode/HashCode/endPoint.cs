f6581ea22c3dc0ce4b0fadcfd1343fee896b873f
Malphegal <nateag_96@hotmail.fr>
2017-02-23 20:24:38 +0100

    e

--- HashCode/HashCode/endPoint.cs
+++ HashCode/HashCode/endPoint.cs
@@ -8,5 +8,22 @@ namespace HashCode
 {
     class endPoint
     {
+        public int nbCache { get; set; }
+        public int[,] latCache { get; set; }
+        public int latDC { get; set; }
+        public int[,] video { get; set; }
+
+        public endPoint(int nbCache, int[,] latCache, int latDC, int[,] video)
+        {
+            this.nbCache = nbCache;
+            this.latCache = latCache;
+            this.latDC = latDC;
+            this.video = video;
+        }
+
+        ~endPoint()
+        {
+
+        }
     }
 }
